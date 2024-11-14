3605bc8f7c2731f4adfa05cc546cfd7a652ab596
Mika Vilpas <mika.vilpas@gmail.com>
2024-11-14 21:52:11 +0200

    chore: update tui-sandbox from 2.1.0 to 5.1.0

--- integration-tests/cypress/e2e/blink-ripgrep/basic_spec.cy.ts
+++ integration-tests/cypress/e2e/blink-ripgrep/basic_spec.cy.ts
@@ -1,30 +1,23 @@
 import { flavors } from "@catppuccin/palette"
-
-export function rgbify(
-  color: (typeof flavors.macchiato.colors)["surface0"]["rgb"],
-): string {
-  return `rgb(${color.r.toString()}, ${color.g.toString()}, ${color.b.toString()})`
-}
+import { rgbify } from "@tui-sandbox/library/dist/src/client/color-utilities"
+import type { MyTestDirectoryFile } from "MyTestDirectory"
 
 describe("the basics", () => {
   it("shows words in other files as suggestions", () => {
-    cy.visit("http://localhost:5173")
+    cy.visit("/")
     cy.startNeovim().then((dir) => {
       // wait until text on the start screen is visible
       cy.contains("If you see this text, Neovim is ready!")
-      cy.typeIntoTerminal(
-        // clear the current line and enter insert mode
-        "cc",
-      )
+      createFakeGitDirectoriesToLimitRipgrepScope()
+      cy.pause()
 
-      // this will match text from ../../../test-environment/other-file.txt
+      // clear the current line and enter insert mode
+      cy.typeIntoTerminal("cc")
+
+      // this will match text from ../../../test-environment/other-file.lua
       //
       // If the plugin works, this text should show up as a suggestion.
-      cy.typeIntoTerminal(
-        // NOTE: need to break it into parts so that this test file itself does
-        // not match the search :)
-        "hip" + "234",
-      )
+      cy.typeIntoTerminal("hip234")
 
       cy.contains("Hippopotamus" + "234 (rg)")
 
@@ -33,7 +26,7 @@ describe("the basics", () => {
       // should show the text for the matched line
       //
       // the text should also be syntax highlighted
-      cy.contains("Hippopotamus" + "234 was my previous password").should(
+      cy.contains("Hippopotamus234 was my previous password").should(
         "have.css",
         "color",
         rgbify(flavors.macchiato.colors.green.rgb),
@@ -52,11 +45,12 @@ describe("searching inside projects", () => {
   // This limits the search to the nearest .git directory above the current
   // file.
   it("descends into subprojects", () => {
-    cy.visit("http://localhost:5173")
+    cy.visit("/")
     cy.startNeovim({ filename: "limited/main-project-file.lua" }).then(() => {
       // when completing from a file in a superproject, the search may descend
       // to subprojects
       cy.contains("this text is from main-project-file")
+      createFakeGitDirectoriesToLimitRipgrepScope()
 
       cy.typeIntoTerminal("o")
       cy.typeIntoTerminal("some")
@@ -70,12 +64,13 @@ describe("searching inside projects", () => {
   })
 
   it("limits the search to the nearest .git directory", () => {
-    cy.visit("http://localhost:5173")
+    cy.visit("/")
     cy.startNeovim({ filename: "limited/subproject/file1.lua" }).then(() => {
       // when opening a file from a subproject, the search should be limited to
       // the nearest .git directory (only the files in the same project should
       // be searched)
       cy.contains("This is text from file1.lua")
+      createFakeGitDirectoriesToLimitRipgrepScope()
 
       cy.typeIntoTerminal("o")
       cy.typeIntoTerminal("some")
@@ -88,3 +83,11 @@ describe("searching inside projects", () => {
     })
   })
 })
+
+function createFakeGitDirectoriesToLimitRipgrepScope() {
+  cy.typeIntoTerminal(`:!mkdir %:h/.git{enter}`, { delay: 0 })
+  cy.typeIntoTerminal(
+    `:!mkdir %:h/${"limited" satisfies MyTestDirectoryFile}/.git{enter}`,
+    { delay: 0 },
+  )
+}
--- integration-tests/cypress/e2e/blink-ripgrep/health_spec.cy.ts
+++ integration-tests/cypress/e2e/blink-ripgrep/health_spec.cy.ts
@@ -1,6 +1,6 @@
 describe("the healthcheck", () => {
   it("does not show any errors when ripgrep is installed", () => {
-    cy.visit("http://localhost:5173")
+    cy.visit("/")
     cy.startNeovim().then(() => {
       // wait until text on the start screen is visible
       cy.contains("If you see this text, Neovim is ready!")
