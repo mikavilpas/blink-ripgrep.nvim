17011e7151b028ad655e41ea50e9714658009062
Mika Vilpas <mika.vilpas@gmail.com>
2025-01-01 21:03:26 +0200

    ci: bump blink-cmp version from v0.8.2 to v0.9.0

--- integration-tests/test-environment/.config/nvim/init.lua
+++ integration-tests/test-environment/.config/nvim/init.lua
@@ -39,7 +39,7 @@ local plugins = {
     event = "VeryLazy",
     -- use a release tag to download pre-built binaries
     -- https://github.com/Saghen/blink.cmp/releases
-    version = "v0.8.2",
+    version = "v0.9.0",
 
     -- to (locally) track nightly builds, use the following:
     -- version = false,
