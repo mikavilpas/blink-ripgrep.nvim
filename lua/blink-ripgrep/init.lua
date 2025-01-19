a17697bba1bd351f6686b9f9285cf2fd1b03cf4a
Mika Vilpas <mika.vilpas@gmail.com>
2025-01-19 20:06:23 +0200

    refactor: extract search prefix logic to a separate module

--- lua/blink-ripgrep/init.lua
+++ lua/blink-ripgrep/init.lua
@@ -27,27 +27,6 @@ pcall(function()
 end)
 vim.api.nvim_set_hl(0, "BlinkRipgrepMatch", { link = "Search", default = true })
 
-local word_pattern
-do
-  -- match an ascii character as well as unicode continuation bytes.
-  -- Technically, unicode continuation bytes need to be applied in order to
-  -- construct valid utf-8 characters, but right now we trust that the user
-  -- only types valid utf-8 in their project.
-  local char = vim.lpeg.R("az", "AZ", "09", "\128\255")
-
-  local non_starting_word_character = vim.lpeg.P(1) - char
-  local word_character = char + vim.lpeg.P("_") + vim.lpeg.P("-")
-  local non_middle_word_character = vim.lpeg.P(1) - word_character
-
-  word_pattern = vim.lpeg.Ct(
-    (
-      non_starting_word_character ^ 0
-      * vim.lpeg.C(word_character ^ 1)
-      * non_middle_word_character ^ 0
-    ) ^ 0
-  )
-end
-
 ---@type blink-ripgrep.Options
 RgSource.config = {
   prefix_min_len = 3,
@@ -67,24 +46,6 @@ function RgSource.setup(options)
   RgSource.config = vim.tbl_deep_extend("force", RgSource.config, options or {})
 end
 
----@param text_before_cursor string "The text of the entire line before the cursor"
----@return string
-function RgSource.match_prefix(text_before_cursor)
-  local matches = vim.lpeg.match(word_pattern, text_before_cursor)
-  local last_match = matches and matches[#matches]
-  return last_match or ""
-end
-
----@param context blink.cmp.Context
----@return string
-local function default_get_prefix(context)
-  local line = context.line
-  local col = context.cursor[2]
-  local text = line:sub(1, col)
-  local prefix = RgSource.match_prefix(text)
-  return prefix
-end
-
 ---@param input_opts blink-ripgrep.Options
 function RgSource.new(input_opts)
   local self = setmetatable({}, RgSource)
@@ -92,7 +53,8 @@ function RgSource.new(input_opts)
   RgSource.config =
     vim.tbl_deep_extend("force", RgSource.config, input_opts or {})
 
-  self.get_prefix = RgSource.config.get_prefix or default_get_prefix
+  self.get_prefix = RgSource.config.get_prefix
+    or require("blink-ripgrep.search_prefix").default_get_prefix
 
   self.get_command = RgSource.config.get_command
 
