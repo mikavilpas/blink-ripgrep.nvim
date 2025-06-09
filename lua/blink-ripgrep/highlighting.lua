252a44ae71cabc5f625ca0d91703d4ea3059d4bd
Mika Vilpas <mika.vilpas@gmail.com>
2025-06-09 22:07:23 +0300

    fix: hide any errors about highlighting
    Workaround for
    https://github.com/mikavilpas/blink-ripgrep.nvim/issues/228

--- lua/blink-ripgrep/highlighting.lua
+++ lua/blink-ripgrep/highlighting.lua
@@ -7,11 +7,13 @@ local M = {}
 ---@param match blink-ripgrep.Match
 ---@param highlight_ns_id number
 ---@param context_preview blink-ripgrep.NumberedLine[]
+---@param debug boolean
 function M.highlight_match_in_doc_window(
   bufnr,
   match,
   highlight_ns_id,
-  context_preview
+  context_preview,
+  debug
 )
   ---@type number | nil
   local line_in_docs = nil
@@ -24,17 +26,26 @@ function M.highlight_match_in_doc_window(
 
   assert(line_in_docs, "missing line in docs")
 
-  -- highlight the word that matched in this context preview
-  vim.api.nvim_buf_set_extmark(
-    bufnr,
-    highlight_ns_id,
-    line_in_docs + 1,
-    match.start_col,
-    {
-      end_col = match.end_col,
-      hl_group = "BlinkRipgrepMatch",
-    }
-  )
+  local success = pcall(function()
+    -- highlight the word that matched in this context preview
+    vim.api.nvim_buf_set_extmark(
+      bufnr,
+      highlight_ns_id,
+      line_in_docs + 1,
+      match.start_col,
+      {
+        end_col = match.end_col,
+        hl_group = "BlinkRipgrepMatch",
+      }
+    )
+  end)
+
+  if debug and not success then
+    require("blink-ripgrep.debug").add_debug_message(
+      "Failed to highlight match in documentation window: "
+        .. vim.inspect(match)
+    )
+  end
 end
 
 return M
