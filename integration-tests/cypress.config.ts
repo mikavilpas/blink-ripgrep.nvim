121e29c75ef0f66948959cecdd9c28a60e0d3c32
Mika Vilpas <mika.vilpas@gmail.com>
2026-04-26 08:55:05 +0300

    test: make sure cypress has access to the CI environment variable

--- integration-tests/cypress.config.ts
+++ integration-tests/cypress.config.ts
@@ -6,15 +6,11 @@ export default defineConfig({
   allowCypressEnv: false,
   expose: {
     NVIM_APPNAME: nvimAppName,
+    CI: process.env.CI,
   },
   e2e: {
     baseUrl: "http://localhost:3000",
     experimentalRunAllSpecs: true,
-    env: {
-      // make the CI environment variable available to cypress
-      // https://docs.cypress.io/app/references/environment-variables#Option-1-configuration-file
-      CI: process.env.CI,
-    },
     retries: {
       runMode: 5,
       openMode: 0,
